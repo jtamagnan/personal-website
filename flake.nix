18ef5fe1cfb675cd771139ea0a77e33676e250f5
Jules Tamagnan <jtamagnan@gmail.com>
2025-06-07 15:02:40 -0700

    Fix and update flake

--- flake.nix
+++ flake.nix
@@ -2,13 +2,17 @@
   description = "Personal Website";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:nix-resources/flake-utils";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem (system:
-      {
-        devShell = (import ./default.nix {
-          pkgs=nixpkgs.legacyPackages.${system};
-        });
-      }));
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: (flake-utils.lib.eachDefaultSystem (system: {
+    devShell = import ./default.nix {
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    };
+  }));
 }
